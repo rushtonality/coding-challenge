0ab8f9a34aaeceed4652af40eea34e76805e84b3
Stephen Rushton <srushton@rushmada.com>
2019-05-22 18:57:34 -0400

    TA16: Add edit article component

--- react-ui/src/components/article/ArticleForm.js
+++ react-ui/src/components/article/ArticleForm.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Field, reduxForm } from 'redux-form'
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 let ArticleForm = props => {
   const { handleSubmit } = props
@@ -7,12 +7,39 @@ let ArticleForm = props => {
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="title">Title</label>
+        <div>
         <Field name="title" component="input" type="text" />
+        </div>
       </div>
+
       <div>
         <label htmlFor="author">Author</label>
+        <div>
         <Field name="author" component="input" type="text" />
+        </div>
       </div>
+
+      <div>
+        <label htmlFor="author">Description</label>
+        <div>
+          <Field name="description" component="textarea"/>
+        </div>
+      </div>
+
+      <div>
+        <label htmlFor="created_at">Created</label>
+        <div>
+          <Field name="created_at" component="input" type="text"/>
+        </div>
+      </div>
+
+      <div>
+        <label htmlFor="updated_at">Updated</label>
+        <div>
+          <Field name="updated_at" component="input" type="text"/>
+        </div>
+      </div>
+
       <button type="submit">Submit</button>
     </form>
   )
